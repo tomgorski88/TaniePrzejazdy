c4c83ba3c6e76b4a3318de09e4df5bf674f5e724
Tomasz Gorski <jomal@vp.pl>
2022-07-15 18:07:51 +0200

    Configured firebase database and verified connection

--- TaniePrzejazdy/MainActivity.cs
+++ TaniePrzejazdy/MainActivity.cs
@@ -1,25 +1,55 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using Firebase;
+using Firebase.Database;
 
 namespace TaniePrzejazdy
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private FirebaseDatabase database;
+        private Button btnTestConnection;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            btnTestConnection = (Button) FindViewById(Resource.Id.mybutton);
+            btnTestConnection.Click += BtnTestConnectionOnClick;
+        }
+
+        private void BtnTestConnectionOnClick(object sender, EventArgs e)
+        {
+            InitializeDatabase();
+        }
+
+        void InitializeDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("tanieprzejazdy-bfab4")
+                    .SetApiKey("AIzaSyAJ3bPs6A49I0g9AcRzx22J-xzGi-PVntM")
+                    .SetDatabaseUrl("https://tanieprzejazdy-bfab4-default-rtdb.europe-west1.firebasedatabase.app")
+                    .SetStorageBucket("tanieprzejazdy-bfab4.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+            }
+            database = FirebaseDatabase.GetInstance(app);
+
+            var dbref = database.GetReference("UserSupport");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short);
         }
     }
 }
