41ee575f6b20e1198864adafd73b4567a1c0d18f
Tomasz Gorski <jomal@vp.pl>
2022-07-15 19:53:16 +0200

    Added register activity layout

--- TaniePrzejazdy/Activities/RegisterActivity.cs
+++ TaniePrzejazdy/Activities/RegisterActivity.cs
@@ -1,16 +1,17 @@
 ﻿using Android.App;
 using Android.OS;
+using AndroidX.AppCompat.App;
 
 namespace TaniePrzejazdy.Activities
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/tpTheme", MainLauncher = false)]
-    public class LoginActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/tpTheme", MainLauncher = true)]
+    public class RegisterActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.login);
+            SetContentView(Resource.Layout.register);
         }
     }
 }
